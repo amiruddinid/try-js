6752c7225b068e67cb1cdd006cbc683a9c9a2f63
Amiruddin Irsyad <amiruddinid@gmail.com>
2022-10-07 17:21:33 +0700

    sort

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,22 +2,53 @@ const app = document.getElementById("app");
 const transmisi = document.getElementsByName('transmisi')[0]
 const tahun = document.getElementsByName('tahun')[0];
 const manufaktur = document.getElementsByName('manufaktur')[0];
+const search = document.getElementById('cari');
+const searchButton = document.getElementById("search");
+let currentSort = '';
 const table = new Table();
 const filter = {
     transmisi: '',
     tahun: '',
-    manufaktur: '',
+    manufaktur: '', 
 }
 
 table.init(app);
 
 table.renderBody(motor);
 
+const tableHeader = document.getElementById('table').querySelectorAll('th');
+
+for (let i = 1; i < tableHeader.length; i++) {
+    tableHeader[i].addEventListener("click", function() {
+        sortMotor(tableHeader[i].textContent.toLowerCase())
+    });
+}
+
+function sortMotor(sortBy){
+    if(currentSort === sortBy){
+        currentSort = ''
+        motor.sort(function(a, b){
+            if(a[sortBy] > b[sortBy]) { return -1; }
+            if(a[sortBy] < b[sortBy]) { return 1; }
+            return 0;
+        })
+    }else{
+        currentSort = sortBy
+        motor.sort(function(a, b){
+            if(a[sortBy] < b[sortBy]) { return -1; }
+            if(a[sortBy] > b[sortBy]) { return 1; }
+            return 0;
+        })
+    }
+
+    table.renderBody(motor);
+}
+
 function filterMotor(){
     const filteredMotor = motor.filter(function(el) {
-        const filterTransmisi = filter.transmisi ? el.transmisi === filter.transmisi : true;
-        const filterTahun = filter.tahun ? el.tahun === filter.tahun : true;
-        const filterManufaktur = filter.manufaktur ? el.manufaktur === filter.manufaktur : true;
+        const filterTransmisi = filter.transmisi ? el.transmisi.includes(filter.transmisi) : true;
+        const filterTahun = filter.tahun ? el.tahun.includes(filter.tahun) : true;
+        const filterManufaktur = filter.manufaktur ? el.manufaktur.includes(filter.manufaktur) : true;
         return filterTransmisi && filterTahun && filterManufaktur 
     });
 
@@ -40,4 +71,16 @@ manufaktur.addEventListener('input', function(event){
     const val = event.target.value
     filter.manufaktur = val
     filterMotor()
+})
+
+searchButton.addEventListener('click', (e) => {
+    const cari = search.value.toLowerCase();
+    const findMotor = motor.filter(o => {
+        return o.nama.toLowerCase().includes(cari) 
+        || o.manufaktur.toLowerCase().includes(cari) 
+        || o.tahun.toLowerCase().includes(cari)
+    })
+    console.log(findMotor, cari)
+    if(!findMotor) return;
+    table.renderBody(findMotor);
 })
